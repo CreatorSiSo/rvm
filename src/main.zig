487db666e948a347d00584d5922ca86480d2059c
Simon Sommer <64036709+CreatorSiSo@users.noreply.github.com>
2024-02-11 01:42:39 +0100

    Read opcodes.

--- src/main.zig
+++ src/main.zig
@@ -1,39 +1,107 @@
 const std = @import("std");
 const io = std.io;
-const print = std.debug.print;
-const Tuple = std.meta.Tuple;
-
-var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-const allocator = gpa.allocator();
+const debug = std.debug;
+const log = std.log;
+const mem = std.mem;
+const Allocator = mem.Allocator;
 
 pub fn main() !void {
+    const scope = log.scoped(.main);
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
     const stdin_file = io.getStdIn().reader();
     var stdin_buffered = io.bufferedReader(stdin_file);
     const stdin = stdin_buffered.reader();
 
-    // const stdout_file = io.getStdOut().writer();
-    // var stdout_buffered = io.bufferedWriter(stdout_file);
-    // const stdout = stdout_buffered.writer();
+    const stdout_file = io.getStdOut().writer();
+    var stdout_buffered = io.bufferedWriter(stdout_file);
+    const stdout = stdout_buffered.writer();
 
-    const version = try parse_version(@TypeOf(stdin), stdin);
-    print("Version {}.{}.{}\n", version);
+    const module = Module.deserialize(allocator, stdin.any()) catch |err| {
+        switch (err) {
+            error.ReadVersion => scope.err("Could not read version", .{}),
+            error.ReadLength => scope.err("Could not read length", .{}),
+            error.ReadOpCode => scope.err("Could not read opcode", .{}),
+        }
+        std.process.exit(1);
+    };
+    defer module.deinit();
+    try module.print(stdout.any());
 
-    const len = try stdin.readInt(u64, std.builtin.Endian.big);
-    const bytes = try stdin.readAllAlloc(allocator, len);
-    print("Read {} bytes\n", .{bytes.len});
+    try stdout_buffered.flush();
+}
 
-    for (bytes) |byte| {
-        print("{}\n", .{byte});
+const FormatVersion = struct {
+    major: u8,
+    minor: u8,
+    patch: u8,
+};
+
+const Module = struct {
+    const Self = @This();
+
+    /// Version of the binary format the Module was deserialized from
+    version: FormatVersion,
+    opcodes: []OpCode,
+    allocator: std.mem.Allocator,
+
+    pub fn deinit(self: Self) void {
+        self.allocator.free(self.opcodes);
     }
-}
 
-const FormatVersion = Tuple(&.{ u8, u8, u8 });
+    const DeserializeError = error{
+        ReadVersion,
+        ReadLength,
+        ReadOpCode,
+    };
+    pub fn deserialize(allocator: std.mem.Allocator, reader: io.AnyReader) DeserializeError!Self {
+        const version = FormatVersion{
+            .major = reader.readByte() catch return error.ReadVersion,
+            .minor = reader.readByte() catch return error.ReadVersion,
+            .patch = reader.readByte() catch return error.ReadVersion,
+        };
 
-fn parse_version(comptime Reader: type, input: Reader) !FormatVersion {
-    const major = try input.readByte();
-    const minor = try input.readByte();
-    const patch = try input.readByte();
-    return .{ major, minor, patch };
-}
+        const len = reader.readInt(u16, std.builtin.Endian.big) catch {
+            return error.ReadLength;
+        };
+        const opcodes = allocator.alloc(OpCode, len) catch {
+            debug.panic("Out of memory", .{});
+        };
+        for (0..len) |i| {
+            opcodes[i] = reader.readStructEndian(OpCode, std.builtin.Endian.big) catch {
+                return error.ReadOpCode;
+            };
+        }
 
-const Module = struct {};
+        return Module{
+            .version = version,
+            .opcodes = opcodes,
+            .allocator = allocator,
+        };
+    }
+
+    pub fn print(self: *const Self, writer: io.AnyWriter) !void {
+        try writer.print("Version {}.{}.{}\n", self.version);
+        try writer.print("opcodes:\n", .{});
+        for (self.opcodes) |opcode| {
+            try writer.print("    [{x:0>2}{x:0>6}] {s}, {}\n", .{
+                @intFromEnum(opcode.kind),
+                opcode.data,
+                @tagName(opcode.kind),
+                opcode.data,
+            });
+        }
+    }
+};
+
+const OpCode = packed struct(u32) {
+    kind: enum(u8) {
+        /// Push value of constant with index `Self.data`
+        LoadConstant = 0x01,
+        /// Push value of `Self.data`
+        LoadInline = 0x02,
+    },
+    data: u24,
+};
